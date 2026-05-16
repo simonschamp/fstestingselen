5597690a8a5e762e63b2ae71491b311602b5a179
Simon Ihegbu <simon.ihegbu@student.lut.fi>
2026-05-17 01:03:00 +0300

    Modified the About page

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -4,10 +4,33 @@ const About = () => {
       <h2>About</h2>
       <p>
         This project is a full-stack project built to be tested with authomated
-        testing tool. The testing tool is integrated with Large Language Models
-        (LLMs). Among many things the testing tool is expected to run different
-        functionalities and then deliver metrics and reports which help us
-        determine the reliability of the application.
+        testing tool: Selenium. Please note that this is the second module of
+        the project. If you need access to both projects, please get intouch
+        with me (simonsheg@outlook.com). The testing tool is integrated with
+        Large Language Models (LLMs). Among many things the testing tool is
+        expected to run different functionalities and then deliver metrics and
+        reports which help us determine the reliability of this web application.
+      </p>
+      <p>
+        PLease register or log in with simple username and password. The app
+        works better when you register and log in.
+      </p>
+
+      <p>
+        This application is built with many features/components with integrated
+        microservices. The functions include: login, register functionalities.
+        Others include, authentication, drag-n-drop, translations with
+        integrated open API for fetching posts and comments from public data,
+        useEffect functions. The database is connectted and running. With a
+        React frontend and NodeJS/Express backend. This is a demo app with
+        minimal curated frontend. <br />
+        You can test the drag-and-drop feature in the "GETFORMINPUT" after
+        adding several messages!
+      </p>
+
+      <p>
+        Your feedback is welcome, you can contact me through:
+        simonsheg@outlook.com
       </p>
     </div>
   );
